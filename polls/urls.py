46081deb5073e01cd56d6d25fb77018dbcd0a0d5
stanislav3316 <stanislav3316@gmail.com>
2016-12-07 21:08:06 +0300

    alpha

--- polls/urls.py
+++ polls/urls.py
@@ -6,22 +6,12 @@ app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<course_id>[0-9]+)/$', views.details, name='detail'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<course_id>[0-9]+)/vote/$', views.addStu, name='vote'),
     # ex: /polls/add/
     url(r'^add/$', views.add, name='add'),
     # ex: /polls/remove/5/
     url(r'^remove/$', views.delete, name='delete'),
-    # ex: /polls/removeStudent/5/course/4/
-    url(r'^removeStudent/$', views.deleteStudent, name='deleteStudent'),
     # ex: /polls/edit/5/
-    url(r'^edit/(?P<goods_id>[0-9]+)$', views.editCourse, name='edit'),
-    # ex: /polls/updateCourse/4/
-    url(r'^updateCourse/(?P<course_id>[0-9]+)$', views.updateCourse, name='updateCourse'),
-    # ex: /polls/editStu/4
-    url(r'^editStu/(?P<student_id>[0-9]+)$', views.editStudent, name='editStu'),
-    # ex: /polls/updateStudent
-    url(r'^updateStudent/(?P<student_id>[0-9]+)/$', views.updateStudent, name='updateStu')
+    url(r'^edit/(?P<goods_id>[0-9]+)$', views.editGoods, name='edit'),
+    # ex: /polls/updateGoods/4/
+    url(r'^updateCourse/(?P<goods_id>[0-9]+)$', views.updateGoods, name='updateGoods'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from .models import Course, Student, Goods
+from .models import Goods
 
 
 # Create your views here.
@@ -16,35 +16,6 @@ def index(request):
         'listOfGoods': listOfGoods
     }
     return HttpResponse(template.render(context, request))
-
-
-def details(request, course_id):
-    try:
-        currentCourse = Course.objects.get(pk=course_id)
-        listOfStudents = currentCourse.student_set.all()
-        mainTitle = "Student list of " + currentCourse.name
-    except Course.DoesNotExist:
-        raise Http404("Course Does not exist")
-    return render(request, 'polls/details.html', {
-        'studentsList': listOfStudents,
-        'mainTitle': mainTitle,
-        'myId': course_id,
-        'desc': currentCourse.description})
-
-
-def addStu(request, course_id):
-    courseCurr = get_object_or_404(Course, pk=course_id)
-    try:
-        courseCurr.student_set.create(name=request.POST['name'],
-                                      family=request.POST['family'])
-    except (KeyError, Course.DoesNotExist):
-        return render(request, 'polls/details.html', {
-            'question': courseCurr,
-            'error_message': "You don't select a choice!"
-        })
-    else:
-        courseCurr.save()
-        return HttpResponseRedirect(reverse('polls:detail', args=(courseCurr.id,)))
 
 
 def add(request):
@@ -61,34 +32,21 @@ def delete(request):
     return HttpResponseRedirect(reverse('polls:index'))
 
 
-def deleteStudent(request):
-    Student.objects.filter(id=request.POST["student_id"]).delete()
-    return HttpResponseRedirect(reverse('polls:detail', args=(request.POST["course_id"],)))
-
-
-def editCourse(request, goods_id):
+def editGoods(request, goods_id):
     goods = get_object_or_404(Goods, id=goods_id)
-    return render(request, 'polls/editCourse.html', {'goods': goods})
+    return render(request, 'polls/edit.html', {'goods': goods})
 
 
-def updateCourse(request, course_id):
-    Goods.objects.filter(id=course_id).update(name=request.POST['name'], count=request.POST['count'],
-                                              address=request.POST['address'],
-                                              date=request.POST['date'],
-                                              isArrive=request.POST['arrived'])
-    return HttpResponseRedirect(reverse('polls:index'))
-
-
-def editStudent(request, student_id):
-    student = get_object_or_404(Student, id=student_id)
-    courses = Course.objects.all()
-    return render(request, 'polls/editStu.html', {
-        'student': student, 'courses': courses})
-
-
-def updateStudent(request, student_id):
-    course = get_object_or_404(Course, name=request.POST['course'])
-    Student.objects.filter(id=student_id).update(name=request.POST['name'],
-                                                 family=request.POST['family'],
-                                                 learnSubj=course)
-    return HttpResponseRedirect(reverse('polls:index'))
+def updateGoods(request, goods_id):
+    goods = get_object_or_404(Goods, id=goods_id)
+    try:
+        goods.name = request.POST['name']
+        goods.count = request.POST['count']
+        goods.address = request.POST['address']
+        goods.date = request.POST['date']
+        goods.isArrive = request.POST['arrived']
+        goods.save()
+    except KeyError:
+        return render(request, 'polls/edit.html', {'goods': goods})
+    else:
+        return HttpResponseRedirect(reverse('polls:index'))
