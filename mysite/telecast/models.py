41221f26caeb1db6c955885f6d2de261a7039bd6
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-12-07 19:19:57 +0300

    model

--- mysite/telecast/models.py
+++ mysite/telecast/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from datetime import date
 
 
-# class Telecast(models.Model):
-#     title = models.CharField(max_length=50)
+class Telecast(models.Model):
+    title = models.CharField(max_length=50)
+    duration = models.IntegerField(default=90)
+    description = models.CharField(max_length=500)
+    broadcastDate = models.DateField(default=date.today)
+    isAdvertising = models.BooleanField(default=False)
