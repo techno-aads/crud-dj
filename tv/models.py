c87a61a7258ef6e905579a7e0f7d8ac9a56d2a7f
mmishak <mmishak.ya@gmail.com>
2016-12-07 21:21:02 +0300

    Fix: IntegerField для длительности изменен на PositiveIntegerField

--- tv/models.py
+++ tv/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Program(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
-    length = models.IntegerField()
+    length = models.PositiveIntegerField()
     date = models.DateField()
     add_advert = models.BooleanField()
 
