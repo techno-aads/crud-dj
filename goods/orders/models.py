9e7d58cb9d10912089bcea0259fd996e5c7cc192
qwisz <barybin.anton@gmail.com>
2016-12-07 21:22:42 +0300

    goods. version 2

--- goods/orders/models.py
+++ goods/orders/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=77)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     address = models.CharField(max_length=150)
     delivery_date = models.DateTimeField('delivery date')
     status = models.BooleanField()
