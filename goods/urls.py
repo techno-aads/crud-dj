6a9fd9a91b971c61b1f17af6c1085524b8b2134a
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2016-12-15 23:09:27 +0300

    add simple login form

--- goods/urls.py
+++ goods/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 
 from goods import views
 
 app_name = 'goods'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', login, {'template_name': 'goods/login.html'}, name='login'),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^create/', views.create, name='create'),
     url(r'^add/$', views.add, name='add'),
     url(r'^(?P<good_id>[0-9]+)/$', views.detail, name='detail'),
