82fc73bc68a7f6b8692cd8fe1bfba40d77e641f3
Georgiy Kubrin <kubrin.eg.s@gmail.com>
2016-12-16 22:40:14 +0300

    Added users

--- work2/tvProgramm/forms.py
+++ work2/tvProgramm/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Programm
+from django.core import validators
+from django.contrib.auth.models import User
 
 class AddProgrammForm(forms.ModelForm):
     class Meta:
@@ -8,4 +10,7 @@ class AddProgrammForm(forms.ModelForm):
         widgets = {
             'length': forms.TimeInput(format='%H:%M'),
         }
-        
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput, label='Password', max_length=100)
--- work2/tvProgramm/urls.py
+++ work2/tvProgramm/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import login, logout
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^signup/$', CreateView.as_view(
+            template_name='edit.html',
+            form_class=UserCreationForm,
+            success_url='/programms')
+            , name='registration'),
+    url(r'^login/$', views.loginView, name='login'),
+    url(r'^logout/$', views.logoutView, name='logout'),
     url(r'^add/$', views.addProgramm, name='add'),
     url(r'^(?P<id>[0-9]*)/save/$', views.saveProgramm, name='save'),
     url(r'^(?P<id>[0-9]*)/delete/$', views.deleteProgramm, name='delete'),
