56a894ad5a21eb48c765c76161786480c74b1f17
Алексей Филиппов <inbox@afilippo.ru>
2016-12-21 23:20:12 +0300

    Added authorization

--- project/app/static/js/main.js
+++ project/app/static/js/main.js
@@ -5,6 +5,10 @@ var app = {
 		if (typeof init[page] == 'function'){
 			init[page]();
 		}
+	},
+	regex: {
+		email: /^[^\@]+@[^\@]+$/,
+		login: /^([a-z0-9]+[\.\-]?){3,}$/i
 	}
 }
 
@@ -15,57 +19,84 @@ var init = {
 	index: function(){
 
 	},
+	login: function(){
+
+		init.func.validForm($('#form-login'), [
+			{
+				$el: $('#input-login'),
+				validate: function(_this){
+					return _this.value.match(app.regex.login);
+				}
+			},{
+				$el: $('#input-password'),
+				validate: function(_this){
+					var value = _this.value;
+					var b = value.match(/[a-z]/);
+					b = b && value.match(/[A-Z]/);
+					b = b && value.match(/[0-9]/);
+					b = b && (value.length > 7);
+					return b;
+				}
+			}
+		]);
+	},
+	registration: function(){
+
+		init.func.validForm($('#registration-form'), [
+			{
+				$el: $('#input-email'),
+				validate: function(_this){
+					return _this.value.match(app.regex.email);
+				}
+			},{
+				$el: $('#input-login'),
+				validate: function(_this){
+					return _this.value.match(app.regex.login);
+				}
+			},{
+				$el: $('#input-password'),
+				validate: function(_this){
+					$('#input-password-repeat').trigger('input');
+					var value = _this.value;
+					var b = value.match(/[a-z]/);
+					b = b && value.match(/[A-Z]/);
+					b = b && value.match(/[0-9]/);
+					b = b && (value.length > 7);
+					return b;
+				}
+			},{
+				$el: $('#input-password-repeat'),
+				validate: function(_this){
+					var value = _this.value;
+					var b = value.match(/[a-z]/);
+					b = b && value.match(/[A-Z]/);
+					b = b && value.match(/[0-9]/);
+					b = b && (value.length > 7);
+					b = b && (value == $('#input-password').val());
+					return b;
+				}
+			}
+		]);
+	},
 	product: function(){
 		$('#input-date').datetimepicker({
 			format: 'DD.MM.YYYY',
 			useCurrent: false
 		});
 
-		var $form = $('#form-product');
-		var $checkValid = $form.find('.check-valid');
-		var $inputs = $checkValid.find('.input-valid');
-
-		var changeClass = function(el, state, addError){
-			var $wrap = $(el).closest($checkValid);
-			$wrap.removeClass('has-success has-error');
-			if (state){
-				$wrap.addClass('has-success');
-			} else if (addError){
-				$wrap.addClass('has-error');
+		init.func.validForm($('#form-product'), [
+			{
+				$el: $('#input-count'),
+				validate: function(_this){
+					return _this.value.match(/^[0-9]+$/);
+				}
+			},{
+				$el: $('#input-name, #input-address, #input-date'),
+				validate: function(_this){
+					return _this.value.trim().length > 1;
+				}
 			}
-		}
-
-		$('#input-name, #input-address, #input-date')
-			.bind('checkValid', function(){
-				changeClass(this, this.value.trim().length > 1, true);
-			})
-			.bind('checkValidWithoutDanger', function(){
-				changeClass(this, this.value.trim().length > 1, false);
-			});
-
-		$('#input-count')
-			.bind('checkValid', function(){
-				changeClass(this, this.value.match(/^[0-9]+$/), true);
-			})
-			.bind('checkValidWithoutDanger', function(){
-				changeClass(this, this.value.match(/^[0-9]+$/), false);
-			});
-
-		$inputs.bind('focusout', function(){
-			$(this).trigger('checkValid');
-		});
-		$inputs.bind('focus input', function(){
-			$(this).trigger('checkValidWithoutDanger');
-		});
-
-		$inputs.trigger('checkValidWithoutDanger');
-
-		$form.bind('submit', function(){
-			$inputs.trigger('checkValid');
-			if ($form.find('.check-valid.has-error').length){
-				return false;
-			}
-		})
+		]);
 
 		var $removeForm = $('#remove-form');
 		var $modal = $('#modal-remove');
@@ -91,6 +122,48 @@ var init = {
 	},
 	products: function(){
 
+	},
+	func: {
+		validForm: function($form, data){
+			var $checkValid = $form.find('.check-valid');
+			var $inputs = $checkValid.find('.input-valid');
+
+			var changeClass = function(el, state, addError){
+				var $wrap = $(el).closest($checkValid);
+				$wrap.removeClass('has-success has-error');
+				if (state){
+					$wrap.addClass('has-success');
+				} else if (addError){
+					$wrap.addClass('has-error');
+				}
+			}
+
+			data.forEach(function(element){
+				element.$el
+					.bind('checkValid', function(){
+						changeClass(this, element.validate(this), true);
+					})
+					.bind('checkValidWithoutDanger', function(){
+						changeClass(this, element.validate(this), false);
+					});
+			})
+
+			$inputs.bind('focusout', function(){
+				$(this).trigger('checkValid');
+			});
+			$inputs.bind('focus input', function(){
+				$(this).trigger('checkValidWithoutDanger');
+			});
+
+			$inputs.trigger('checkValidWithoutDanger');
+
+			$form.bind('submit', function(){
+				$inputs.trigger('checkValid');
+				if ($form.find('.check-valid.has-error').length){
+					return false;
+				}
+			})
+		}
 	}
 }
 
--- project/app/urls.py
+++ project/app/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login$', views.auth, name='login'),
+    url(r'^logout$', views.auth_out, name='logout'),
+    url(r'^registration$', views.registration, name='registration'),
+    url(r'^registration-success$', views.registration_success, name='registration_success'),
     url(r'^products$', views.products, name='products'),
     url(r'^products/(?P<product_id>[0-9]+)$', views.product, name='product'),
 	url(r'^products/save$', views.save_product, name='save_product'),
