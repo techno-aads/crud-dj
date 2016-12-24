9a1fa370cff6d5d18361d57aefbfc63f46653a50
alsmirnova <alyona_smir@mail.ru>
2016-12-24 04:11:11 +0300

    Add authorization
    Add authorization, customization and some functionality

--- newsite2/Orders/urls.py
+++ newsite2/Orders/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
@@ -6,7 +6,10 @@ app_name='Orders'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<order_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>[0-9]+)/edit/$', views.edit, name='edit'),
-	url(r'^(?P<question_id>[0-9]+)/save/$', views.save, name='save')
+	url(r'^(?P<order_id>[0-9]+)/edit/$', views.edit, name='edit'),
+	url(r'^(?P<order_id>[0-9]+)/save/$', views.save, name='save'),
+	url(r'^new/$', views.new, name='new'),
+	url(r'^login/$', views.LoginFormView.as_view(), name='login'),
+	url(r'^logout/$', views.logout_view, name='logout')
 	]
 	
--- newsite2/Orders/views.py
+++ newsite2/Orders/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.http import Http404
 from django.urls import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.edit import FormView
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.conf import settings
+
+import datetime
 
 from .models import Status, Order
 
@@ -18,25 +25,70 @@ def detail(request, order_id):
 	return render(request, 'Orders/detail.html',{'order': order})
 	
 
-def edit(request, id):
-  order = get_object_or_404(Order, pk=id)
-  return render(request, 'edit.html', {'order': order})
-   
+def edit(request, order_id):
+	if not request.user.is_authenticated:
+		return HttpResponseRedirect(reverse('Orders:login'))
+	else:
+		order = get_object_or_404(Order, pk=order_id)
+		status_list = Status.objects.order_by('-name')
+		return render(request, 'Orders/edit.html',{'order': order, 'status_list': status_list,})
 
-def save(request, id):
-    order = get_object_or_404(Order, pk=id)
-    try:
-        order.count=request.POST['count']
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {'order': order})
-    else:
-        order.save()
+
+def save(request, order_id):
+	order = get_object_or_404(Order, pk=order_id)
+	status_list = Status.objects.order_by('-name')
+	try:
+		order.count=int(request.POST['count'])
+		order.address=request.POST['address']
+		#order.date=datetime.datetime.now()
+		#order.status=request.POST['status']
+	except (KeyError,ValueError):
+		# Redisplay the question voting form.
+		return render(request, 'Orders/edit.html', {
+			'order': order,
+			'status_list': status_list,
+			'error_message': "Uncorrect data input!",})
+	else:
+		#order.count = request.POST
+		order.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('detail.html'))   
-   
-   
+		return HttpResponseRedirect(reverse('Orders:detail', args=(order.id,)))
 
-   
+def new(request):
+	if not request.user.is_authenticated:
+		return HttpResponseRedirect(reverse('Orders:login'))
+	else:
+		status_list = Status.objects.order_by('-name')
+		try:
+			new_order=Order(title=request.POST['title'],count=int(request.POST['count']),address=request.POST['address'],date=request.POST['date'],status=request.POST['status'])
+		except (KeyError,ValueError):
+		# Redisplay the question voting form.
+			return render(request, 'Orders/new.html', {
+				'status_list': status_list,
+				'error_message': "Uncorrect data input!",})
+		else:
+		#order.count = request.POST
+			new_order.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+			return HttpResponseRedirect(reverse('Orders:index'))
+		
+		
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    template_name = "Orders/login.html"
+    success_url = "/Orders/"
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('Orders:index'))
