e4d5dcd19880874848bd730d20d77754f38638d5
qwisz <barybin.anton@gmail.com>
2016-12-24 23:27:02 +0300

    add auth

--- goods/orders/urls.py
+++ goods/orders/urls.py
@@ -6,8 +6,12 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^register/$', views.RegisterFormView.as_view(), name='register'),
+    url(r'^login/$', views.LoginFormView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^create/$', views.ProductCreate.as_view(), name='create'),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.ProductDelete.as_view(), name='delete'),
     url(r'^(?P<pk>[0-9]+)/update/$', views.ProductUpdate.as_view(), name='update'),
+
 ]
--- goods/orders/views.py
+++ goods/orders/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.http import Http404
@@ -17,6 +20,11 @@ class IndexView(generic.ListView):
         """Return the last ten products."""
         return Product.objects.order_by('-name')[:10]
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
+
 
 class DetailView(generic.DetailView):
     model = Product
@@ -29,11 +37,23 @@ class ProductCreate(generic.CreateView):
     fields = ['name', 'quantity', 'address', 'delivery_date', 'status']
     success_url = reverse_lazy('orders:index')
 
+    def get(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse_lazy('orders:login'))
+        else:
+            return super(ProductCreate, self).get(request, args, kwargs)
+
 
 class ProductDelete(generic.DeleteView):
     template_name_suffix = '_delete'
     model = Product
     success_url = reverse_lazy('orders:index')
+
+    def get(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse_lazy('orders:login'))
+        else:
+            return super(ProductDelete, self).get(request, args, kwargs)
 
 
 class ProductUpdate(generic.UpdateView):
@@ -42,3 +62,35 @@ class ProductUpdate(generic.UpdateView):
     fields = ['name', 'quantity', 'address', 'delivery_date', 'status']
     success_url = reverse_lazy('orders:index')
 
+    def get(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse_lazy('orders:login'))
+        else:
+            return super(ProductUpdate, self).get(request, args, kwargs)
+
+class RegisterFormView(generic.FormView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('orders:login')
+    template_name = 'orders/register.html'
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+
+class LoginFormView(generic.FormView):
+    form_class = AuthenticationForm
+    template_name = 'orders/login.html'
+    success_url = reverse_lazy('orders:index')
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+class LogoutView(generic.View):
+
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse_lazy('orders:index'))
