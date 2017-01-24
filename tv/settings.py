c02a80cfba8a838c9ab4038acb8a240daf5c067c
Igor <igozzz94@gmail.com>
2017-01-25 01:28:14 +0300

    settings updated

--- tv/settings.py
+++ tv/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
+    'login_app',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,10 @@ ROOT_URLCONF = 'tv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'app/templates'),
+            os.path.join(BASE_DIR, 'login_app/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tv/urls.py
+++ tv/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^app/', include('app.urls')),
+    url(r'^login_app/', include('login_app.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('app.urls')),
 ]
