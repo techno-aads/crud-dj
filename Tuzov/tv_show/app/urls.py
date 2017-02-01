7101a918460257723ca5898ff51ce9553ea76bbb
greg <greg.tuzov@gmail.com>
2017-02-02 01:04:48 +0300

    add sign in

--- Tuzov/tv_show/app/urls.py
+++ Tuzov/tv_show/app/urls.py
@@ -1,14 +1,25 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'app'
 urlpatterns = [
+
+    url(r'^accounts/login/$', auth_views.login),
+
     # ex: /app/
     url(r'^$', views.index, name='index'),
     # ex: /app/5/
-    url(r'^(?P<tv_show_id>[0-9]+)/$', views.detail, name='detail'),
+    #url(r'^(?P<tv_show_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /app/add/
     url(r'^add/$', views.add, name='add'),
+
+    url(r'^register/', views.register, name='register'),
+    url(r'^userLogin/', views.userLogin, name='userLogin'),
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^edit/', views.edit, name='edit'),
+    url(r'^login/', views.login, name='login')
+
 
 
     # ex: /polls/5/
--- Tuzov/tv_show/app/views.py
+++ Tuzov/tv_show/app/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
+from django.template.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
 from django.urls import reverse
 from django.http import Http404
 from django.views import generic
@@ -12,8 +17,11 @@ from .models import TVShow
 
 def index(request):
     template = loader.get_template('app/index.html')
-    tv_show_list = TVShow.objects.all()
-    context = {'tv_show_list': tv_show_list}
+    tv_show_list = TVShow.objects.order_by('broadcast_date')
+    context = {
+        'tv_show_list': tv_show_list,
+        'username': auth.get_user(request).username
+    }
     return HttpResponse(template.render(context, request))
 
 
@@ -38,5 +46,59 @@ def add(request):
     else:
         msg = "you submitted an empty or part-empty form"
     return render(request, 'app/add_success.html', {})
+
+
+def login(request):
+    template = loader.get_template('app/login.html')
+    return HttpResponse(template.render({}, request))
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('app:index'))
+
+
+def register(request):
+    args = {}
+    args.update(csrf(request))
+    args['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],
+                                        password=newuser_form.cleaned_data['password2'])
+            auth.login(request, newuser)
+            return HttpResponseRedirect(reverse('tvshows:index'))
+        else:
+            args['form'] = newuser_form
+    template = loader.get_template('tvshows/register.html')
+    return HttpResponse(template.render(args, request))
+
+
+def userLogin(request):
+    login = request.POST['login']
+    password = request.POST['pass']
+    user = auth.authenticate(username=login, password=password)
+    if user is not None:
+        auth.login(request, user)
+
+        return HttpResponseRedirect(reverse('app:index'))
+    else:
+        template = loader.get_template('app/login.html')
+        return HttpResponse(template.render({'error': 'Wrong login or password'}, request))
+
+
+@login_required(login_url='app:login.html')
+def edit(request):
+    list_shows = TVShow.objects.order_by('broadcast_date')
+    template = loader.get_template('app/edit.html')
+    context = {
+        'list_shows': list_shows,
+        'username': auth.get_user(request).username,
+        'form': ShowForm,
+    }
+    return HttpResponse(template.render(context, request))
+
 
     
