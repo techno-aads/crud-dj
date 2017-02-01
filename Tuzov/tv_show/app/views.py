452621a080504e13a5ee81b45c95671908857422
greg <greg.tuzov@gmail.com>
2017-02-01 20:18:02 +0300

    add table view in index.html

--- Tuzov/tv_show/app/views.py
+++ Tuzov/tv_show/app/views.py
@@ -9,15 +9,18 @@ from django.views import generic
 
 from .models import TVShow
 
+
 def index(request):
     template = loader.get_template('app/index.html')
     tv_show_list = TVShow.objects.all()
     context = {'tv_show_list': tv_show_list}
     return HttpResponse(template.render(context, request))
 
+
 def detail(request, tv_show_id):
     tv_show = get_object_or_404(TVShow, pk=tv_show_id)
     return render(request, 'app/detail.html', {'tv_show': tv_show})
+
 
 def add(request):
     if ('name' in request.POST) and ('dur' in request.POST) and ('descr' in request.POST) and ('date' in request.POST): #default values?!
@@ -35,3 +38,5 @@ def add(request):
     else:
         msg = "you submitted an empty or part-empty form"
     return render(request, 'app/add_success.html', {})
+
+    
